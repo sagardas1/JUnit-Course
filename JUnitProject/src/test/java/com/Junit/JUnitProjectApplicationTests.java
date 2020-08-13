a660363024f452d22f8d5c1138d460700909cf80
sagardas1 <sagardas.195@gmail.com>
2020-08-13 17:38:23 +0530

    Junit first project.

---
+++ JUnitProject/src/test/java/com/Junit/JUnitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
