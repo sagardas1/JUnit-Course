71352368b14281dcb57e94ed0f1a9fa759f4c70f
sagardas1 <sagardas.195@gmail.com>
2020-10-02 22:47:03 +0530

    changes

--- JUnitProject/src/main/java/com/Junit/mathUtill/MathUtill.java
+++ JUnitProject/src/main/java/com/Junit/mathUtill/MathUtill.java
@@ -1,15 +1,25 @@
 package com.Junit.mathUtill;
 
 public class MathUtill {
-	
-	
-	public int add(int a,int b) {return a+b;}
-	
-	public int sub(int a,int b) {return a-b;}
-	
-	public int mul(int a,int b) {return a*b;}
-	
-	public int div(int a,int b) {return a/b;}
-	public int additionArray(int a,int b) {return a/b;}
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int sub(int a, int b) {
+		return a - b;
+	}
+
+	public int mul(int a, int b) {
+		return a * b;
+	}
+
+	public int div(int a, int b) {
+		return a / b;
+	}
+
+	public int additionArray(int a, int b) {
+		return a / b;
+	}
 
 }
